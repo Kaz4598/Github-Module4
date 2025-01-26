d69e88e11b11995b414b55cf4c057bad210c20d3
Igbalaye <igbalayek@gmail.com>
2025-01-26 17:55:09 +0000

    init commit

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1{
+    public static void main(String[] args) {
+        System.out.println("Hello world, I am learning Java");
+        System.out.println("Hope to get better");
+        System.out.println("Though it appears difficult,but not giving up");
+        System.out.println("This is the time for patience and concentration");
+        System.out.println("I will be better anyways");
+    }
+}
+
---
+++ src/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte age = -127;
+        short money = 32_76;
+        long soMuchMoney = -2147483649l;
+        float area = 1234567.90f;
+        double biggerArea = 123456.67;
+        System.out.println(area );
+        char option = 'k';
+        boolean isAround = false;
+
+    }
+}
