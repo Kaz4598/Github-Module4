277a5e39b608c235f507216f5a7420eaf1ff5627
Igbalaye <Igbalayek@gmail.com>
2025-01-27 12:35:39 +0000

    Task4Arithmetic.java

---
+++ src/Task4Arithmetic.java
@@ -0,0 +1,24 @@
+public class Task4Arithmetic {
+    public static void main(String[] args) {
+        // Basic arithmetic
+        double age1 = 20;
+        double age2 = 30;
+
+        System.out.println(age1);
+        //System.out.println(age1+age2);
+
+        // incremental and decremental ++ --
+       // age1 --;
+       // System.out.println(age1);
+
+        // Compound assignment operators -= += /= x=
+       // age1 -= age2;
+       // System.out.println(age1);
+        //age1 /= age2;
+        //System.out.println(age1);
+
+
+
+
+    }
+}
