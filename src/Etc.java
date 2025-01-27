82c5401cd6d84078b9396807a64b920f918174d4
Igbalaye <Igbalayek@gmail.com>
2025-01-27 21:44:35 +0000

    modified task2

---
+++ src/Etc.java
@@ -0,0 +1,27 @@
+public class Etc {
+    public static void main(String[] args) {
+        // String and string concat
+       // String camel = "desert camel";
+       // String adjective = "is powerful";
+       // String newString = "camel + adjective";
+       // System.out.println(newString);
+
+        // String and primitive concat
+        //String movieName = "anaconda: ";
+       // int parts = 4;
+       // boolean isReleased = false;
+        //System.out.println(""+parts + isReleased);
+
+        // .concat() method
+        //System.out.println(movieName.concat(parts+""));
+
+        //
+
+
+
+
+
+
+
+    }
+}
--- src/Task2.java
+++ src/Task2.java
@@ -1,13 +1,27 @@
 public class Task2 {
     public static void main(String[] args) {
-        byte age = -127;
-        short money = 32_76;
-        long soMuchMoney = -2147483649l;
-        float area = 1234567.90f;
-        double biggerArea = 123456.67;
-        System.out.println(area );
-        char option = 'k';
+       // byte age = -127;
+       // short money = 32_76;
+       // long soMuchMoney = -2147483649l;
+      //  float area = 1234567.90f;
+       // double biggerArea = 123456.67;
+       // System.out.println(area );
+       // char option = 'k';
+       // boolean isAround = false;
+
+        // Task 2 print out all data types on your console
+        byte age = -60;
+        short money = 19_20;
+        long soMuchMoney = -1247385589l;
+        float area = 2065437.85f;
+        double biggerArea = 206543.56;
+        System.out.println(area);
+        char option = 'l';
         boolean isAround = false;
 
+
     }
-}
+
+    }
+
+
