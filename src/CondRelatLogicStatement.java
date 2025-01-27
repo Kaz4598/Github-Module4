951e1cf10fda64e0c5b5a5894bbdf38823d6dcbe
Igbalaye <Igbalayek@gmail.com>
2025-01-27 17:31:19 +0000

    init commit

---
+++ src/CondRelatLogicStatement.java
@@ -0,0 +1,17 @@
+public class CondRelatLogicStatement {
+
+    public static void main(String[] args) {
+        // Conditional statement
+        int myAge = 20;
+        //if(condition)
+
+        if(myAge == 20){
+            System.out.println(" it is equal");
+        }
+
+    }
+
+
+
+        // Relational operator == != < > <= >=
+}
