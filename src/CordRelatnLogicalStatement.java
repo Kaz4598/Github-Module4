69ac6d7436c44a1a9c8e7f5d6b8bfcb5095dcf00
Igbalaye <Igbalayek@gmail.com>
2025-01-27 19:55:18 +0000

    string operation

---
+++ src/CordRelatnLogicalStatement.java
@@ -0,0 +1,17 @@
+public class CordRelatnLogicalStatement {
+    public static void main(String[] args) {
+        // conditional statements
+        //if (conditions)
+        int myAge = 20;
+        int myScore = 15;
+        if (myAge == 30 && myAge < 35 ){
+            System.out.println(" it is equal for if");
+        }else if (myAge < 30){
+            System.out.println("it is equal for else if ");
+
+        }else {
+            System.out.println("no true condition");
+        }
+        System.out.println(" This is outside the condition");
+    }
+}
---
+++ src/StringOperations.java
@@ -0,0 +1,19 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        String word = "orGANization";
+
+        // upper case and lower case
+        System.out.println(word.toUpperCase());
+        // 0  1  2  3  4  5
+
+        // l  e  n  g  t  h
+        //-6 -5 -4 -3 -2 -1
+
+        // length
+        System.out.println(word.length());
+
+        //index of character
+
+
+    }
+}
